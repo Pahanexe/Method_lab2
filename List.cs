d75bd21500ccb0c744d74dca267221dbc0dac344
pahanexe <paha.oleinik@gmail.com>
2025-05-11 01:57:55 +0300

    Added class "List"

---
+++ List.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_lab2
+{
+    internal class List
+    {
+        private class ListNode
+        {
+            public char data;
+            public ListNode next;
+        }
+        private ListNode head;
+
+
+    }
+}
