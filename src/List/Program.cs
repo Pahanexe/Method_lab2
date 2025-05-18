df8454da2539907b75d54911f9352c6391123050
pahanexe <paha.oleinik@gmail.com>
2025-05-18 17:33:31 +0300

    Changed from Framework to core

---
+++ src/List/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Method_lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListLab list = new ListLab();
+            list.Append('a');
+            list.Append('b');
+            list.Append('c');
+            list.Append('c');
+            list.Append('d');
+            list.display();
+
+            list.reverse();
+            list.display();
+
+
+            Console.WriteLine("List length: " + list.lenght());
+            Console.ReadKey();
+        }
+    }
+}
--- tests/ListTest/UnitTest1.cs
+++ tests/ListTest/UnitTest1.cs
@@ -1,12 +1,3 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Method_lab2;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-//Unit test for ListLab class
-
 namespace Method_lab2.tests
 {
     [TestClass]
