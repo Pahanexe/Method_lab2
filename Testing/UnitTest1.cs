260cf114ff7523438c914be434aeaa6be36d3483
pahanexe <paha.oleinik@gmail.com>
2025-05-18 15:09:20 +0300

    Strat UnitTest

---
+++ Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Method_lab2;
+namespace Method_lab2.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
